5f901b017f489bb25f2708141efe17256bfca6be
karan sharma <karan.sharma516@yahoo.com>
2015-01-29 17:04:25 -0800

    Created GUI

--- src/Program.java
+++ src/Program.java
@@ -1,14 +1,44 @@
 /**
- * @author karan
+ * @author Tom Tsiliopoulos
  *
  */
+import javax.swing.JOptionPane;
+
+
+
 public class Program {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String firstNumber = "";
+		String secondNumber = "";
+		
+		try {
+			
+			firstNumber = JOptionPane.showInputDialog("Enter First Integer");
+			System.out.println(firstNumber);
+			secondNumber = JOptionPane.showInputDialog("Enter Second Integer");
+			System.out.println(secondNumber);
+			
+			int number1 = Integer.parseInt(firstNumber);
+			int number2 = Integer.parseInt(secondNumber);	
+			
+			int sum = number1 + number2;
+			
+			JOptionPane.showMessageDialog(null, "The Sum is " + sum, "Sum of two integers", JOptionPane.PLAIN_MESSAGE);
+			
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Incorrect Values Entered", "Entry Error", JOptionPane.ERROR_MESSAGE);
+			System.out.println(e);
+		}
+		
+		
+		
+		
+		
+		
 
 	}
 
